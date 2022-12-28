982049eae8544e4c891bdaaf84ef2233a736f578
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2022-12-28 14:11:13 -0400

    first commit

---
+++ src/HelloWorld.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Class
+class HelloWorld extends React.Component {
+  render() {
+    return <h1>Hello World!</h1>;
+  }
+}
+
+// Function
+// const HelloWorld = () => {
+//   return <h1>Hello World!</h1>;
+// };
+
+export default HelloWorld;
